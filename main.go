9d4f493740dadb46bace43928f1f70b857f89cac
Isaque Franklin Amaral <isaquefrankli@gmail.com>
2024-05-08 15:16:44 -0300

    pickWinner func and Goroutines + channels test

--- main.go
+++ main.go
@@ -176,3 +176,62 @@ func handleConn(conn net.Conn){
   } 
 
 }
+
+//pickWinner creates a lottery pool of validators and chooses the validator who gets to forge a block to blockchain by random selecting from the pool, weighted by amount of tokens staked.
+func pickWinner(){
+  times.Sleep(30 * time.Second)
+  mutex.Lock()
+  temp := tempBlocks
+  mutex.Unlock()
+
+  lotteryPool := []string{}
+  if len(temp) > 0 {
+    // slightly modified traditional proof of stake algorithm
+		// from all validators who submitted a block, weight them by the number of staked tokens
+		// in traditional proof of stake, validators can participate without submitting a block to be forged
+    OUTER:
+      for _, block := range temp {
+        //if alrealdy in lottery pool, skip
+        for _, node := range lotteryPool {
+          if block.Validator == node {
+            continue OUTER
+          }
+        }
+
+        //lock list of validators to prevent data race 
+        mutex.Lock()
+        setValidators := validators 
+        mutex.Unlock()
+
+        k, ok := setValidators[block.Validator]
+        if ok {
+          for i := 0; i < k; i++ {
+            lotteryPool = append(lotteryPool, block.Validator)
+          }
+        }
+      }
+
+      //randomly pick winner from lotteryPool
+      s := rand.NewSource(time.Now().Unix())
+      r := rand.New(s)
+      lotteryWinner := lotteryPool[r.Intn(len(lotteryPool))]
+
+      //add block of winner to blockchain and let all the other nodes know
+      for _, block := range temp {
+        if block.Validator == lotteryWinner {
+          mutex.Lock()
+          Blockchain = append(Blockchain, block)
+          mutex.Unlock()
+          for _ = range validators {
+            announcements <- "\nwinning validator: " + lotteryWinner + "\n"
+          }
+          break
+        }
+      }
+  }
+
+  mutex.Lock()
+  tempBlocks = []block{}
+  mutex.Unlock()
+}
+//We pick a winner every 30 seconds to give time for each validator to propose a new block.
---
+++ test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func sender(out chan<- int) {
+	for i := 1; i <= 5; i++ {
+		fmt.Println("Enviando:", i)
+		out <- i // Enviando o número para o canal
+		time.Sleep(time.Second) // Espera um segundo antes de enviar o próximo número
+	}
+	close(out) // Fechando o canal após enviar todos os números
+}
+
+func receiver(in <-chan int, done chan<- bool) {
+	for num := range in { // Recebendo os números do canal até que o canal seja fechado
+		fmt.Println("Recebido:", num)
+	}
+	done <- true // Enviando sinal para o canal 'done' indicando que a recepção foi concluída
+}
+
+func main() {
+	ch := make(chan int) // Canal para enviar números da goroutine 'sender' para a goroutine 'receiver'
+	done := make(chan bool) // Canal para sinalizar que a recepção foi concluída
+
+	go sender(ch) // Iniciando a goroutine 'sender' para enviar números para o canal
+	go receiver(ch, done) // Iniciando a goroutine 'receiver' para receber números do canal
+
+	<-done // Esperando até que a recepção seja concluída
+	fmt.Println("Comunicação concluída.")
+}
